71163b228edbdcc4d57a7611c0f94f372e42e62a
Rowan Border <rowanjborder@gmail.com>
2025-02-06 12:27:56 +0200

    small bugfix and new launchfile

--- launch/run_lio_sam_default.launch.py
+++ launch/run_lio_sam_default.launch.py
@@ -4,22 +4,35 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
-
+from launch.conditions import IfCondition
 
 def generate_launch_description():
 
     share_dir = get_package_share_directory('liorf')
-    parameter_file = LaunchConfiguration('params_file')
     rviz_config_file = os.path.join(share_dir, 'rviz', 'mapping.rviz')
+
+    rviz_use = LaunchConfiguration('rviz')
+    parameter_file = LaunchConfiguration('params_file')
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
     params_declare = DeclareLaunchArgument(
         'params_file',
         default_value=os.path.join(
             share_dir, 'config', 'lio_sam_default.yaml'),
         description='FPath to the ROS2 parameters file to use.')
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time', default_value='false',
+        description='Use simulation (Gazebo) clock if true'
+    )
+    declare_rviz_cmd = DeclareLaunchArgument(
+        'rviz', default_value='true',
+        description='Use RViz to monitor results'
+    )
 
     return LaunchDescription([
         params_declare,
+        declare_use_sim_time_cmd,
+        declare_rviz_cmd,
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
@@ -31,28 +44,35 @@ def generate_launch_description():
             package='liorf',
             executable='liorf_imuPreintegration',
             name='liorf_imuPreintegration',
-            parameters=[parameter_file],
-            output='screen'
+            parameters=[parameter_file, 
+                    {'use_sim_time': use_sim_time}],
+            output='screen',
+            prefix=['nice -n 20']
         ),
         Node(
             package='liorf',
             executable='liorf_imageProjection',
             name='liorf_imageProjection',
-            parameters=[parameter_file],
-            output='screen'
+            parameters=[parameter_file, 
+                    {'use_sim_time': use_sim_time}],
+            output='screen',
+            prefix=['nice -n 20']
         ),
         Node(
             package='liorf',
             executable='liorf_mapOptmization',
             name='liorf_mapOptmization',
-            parameters=[parameter_file],
-            output='screen'
+            parameters=[parameter_file, 
+                    {'use_sim_time': use_sim_time}],
+            output='screen',
+            prefix=['nice -n 20']
         ),
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
             arguments=['-d', rviz_config_file],
-            output='screen'
+            output='screen',
+            condition=IfCondition(rviz_use)
         )
     ])
