6132e3a83b1b4dcb4b5eef77d2434d5ee33db247
Rowan Border <rowanjborder@gmail.com>
2025-06-23 22:57:37 +0300

    update map frame

--- launch/run_lio_sam_default.launch.py
+++ launch/run_lio_sam_default.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
-            arguments='0.0 0.0 0.0 0.0 0.0 0.0 map_liosam odom_liosam'.split(' '),
+            arguments='0.0 0.0 0.0 0.0 0.0 0.0 1.0 odom_liosam map_liosam'.split(' '),
             parameters=[parameter_file],
             output='screen'
         ),
